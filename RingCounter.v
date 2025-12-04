4ec3f2b061b7a979f94f40c07b70d3eff847f6b8
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-12-04 22:13:44 +0530

    Update RingCounter.v

--- RingCounter.v
+++ RingCounter.v
@@ -1,11 +1,31 @@
-module ring(input clk,output reg[3:0]q=1000
-);
-always @(posedge clk) begin
-case (q)
-4'b1000:q=4'b0100;
-4'b0100:q=4'b0010;
-4'b0010:q=4'b0001;
-4'b0001:q=4'b1000;
-endcase
-end
+module ring(
+    input clk,
+    output reg [3:0] q = 4'b1000
+    );
+
+    always @(posedge clk) begin
+        case(q)
+            4'b1000: q <= 4'b0100;
+            4'b0100: q <= 4'b0010;
+            4'b0010: q <= 4'b0001;
+            4'b0001: q <= 4'b1000;
+        endcase
+    end
+
 endmodule
+
+// ANOTHER WAY
+
+module ring_counter(
+    input clk,
+    output reg [3:0] q
+    );
+
+    initial q = 4'b0001;  // Starting pattern
+
+    always @(posedge clk) begin
+        q <= {q[2:0], q[3]};  // Rotate left
+    end
+
+endmodule
+
