d5ba14dd154fb7a7823737c54bb1409ed7dac5a1
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-12-04 21:37:10 +0530

    Update SR_FlipFlop.v

--- SR_FlipFlop.v
+++ SR_FlipFlop.v
@@ -6,17 +6,15 @@ module srff(
     output reg qb
     );
 
-    // Initialize q
     initial q = 1'b0;
 
     always @(posedge clk) begin
-        case ({s, r})
-            2'b00: q = q;       // No change
-            2'b01: q = 1'b0;    // Reset
-            2'b10: q = 1'b1;    // Set
-            2'b11: q = 1'bx;    // Invalid condition
+        case({s,r})
+            2'b00: q <= q;     // No change
+            2'b01: q <= 1'b0;  // Reset
+            2'b10: q <= 1'b1;  // Set
+            2'b11: q <= 1'bx;  // Invalid
         endcase
-        qb = ~q;                // Complement output
+        qb <= ~q;
     end
-
 endmodule
