acfa7ccde72bba0f5ea6c526a54ded8d9e8cb269
Joe <127773439+joegeorge022@users.noreply.github.com>
2025-12-04 21:38:36 +0530

    Update T_FF.v

--- T_FF.v
+++ T_FF.v
@@ -4,12 +4,16 @@ module tff(
     output reg q,
     output reg qb
     );
-    initial q= 1'b0;
-    always@(posedge clk)begin
-    case ({t})
-        1'b0: q=0;
-        1'b1: q=~q;
-    endcase
-        qb = ~q;
+
+    initial q = 1'b0;
+
+    always @(posedge clk) begin
+        case(t)
+            1'b0: q <= q;    // Hold state
+            1'b1: q <= ~q;   // Toggle state
+        endcase
+        qb <= ~q;
     end
+
 endmodule
+
